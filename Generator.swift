f724529d3cd51a6792669262935b9ebef7d811f0
TheMechanicX32 <43876390+TheMechanicX32@users.noreply.github.com>
2018-10-08 16:07:55 +0300

    Update Generator.swift

--- Generator.swift
+++ Generator.swift
@@ -1,124 +1,15 @@
 //
 //  Generator.swift
-//  Dungeon Pirates
-//
-//  Created by HuckStudio on 10/1/18.
-//  Copyright © 2018 HucksCorp. All rights reserved.
-//
+//  
+
 
 import Foundation
 import SpriteKit
 
-// Store the x and y values of the room.width and room.height so they can be accessed to draw hallways
+// Store x and y values to generate a hallway
 struct point {
     var x: Int
     var y: Int
-}
-// A class to help translate generated data into a visual tile map
-class TileEngine: SKNode {
-    
-    var rooms = [Room]()
-    var hallways = [Room]()
-    var leaves = [Leaf]()
-    
-    init(tileSize: CGSize, columns: Int, rows: Int) {
-        super.init()
-        
-        rooms = [];
-        hallways = [];
-        leaves = [];
-        
-        let maxLeafSize = 20
-        
-        // First, create leaf to be root of all leaves
-        let root = Leaf(X: 0, Y: 0, W: columns, H: rows);
-        leaves.append(root)
-        
-        var didSplit:Bool = true;
-        
-        // Loop through every Leaf in array until no more can be split
-        while (didSplit) {
-            didSplit = false;
-            for leaf in leaves {
-                if leaf.leftChild == nil && leaf.rightChild == nil { // If not split
-                    // If this leaf is too big, or 75% chance
-                    if leaf.width > maxLeafSize || leaf.height > maxLeafSize || Int.random(in: 0..<100) > 25 {
-                        if (leaf.split()) { // split the leaf
-                            // If split worked, push child leaves into array
-                            leaves.append(leaf.leftChild!)
-                            leaves.append(leaf.rightChild!)
-                            didSplit = true
-                        }
-                    }
-                }
-            }
-        }
-        // Next, iterate through each leaf and create room in each one
-        root.createRooms()
-        
-        for leaf in leaves {
-            // Then draw room and hallway (if there is one)
-            if leaf.room != nil {
-                drawRoom(roomRect: leaf.room!)
-            }
-            if leaf.hallways.isEmpty != true {
-                drawHall(hallRect: leaf.hallways)
-            }
-        }
-        // Initialize a tile map and give it content to build with
-        
-        // A 32x32 black texture
-        let tile1 = SKTexture(imageNamed: "black")
-        
-        // A 32x32 red texture
-        let tile2 = SKTexture(imageNamed: "red")
-        
-        let black = SKTileDefinition(texture: tile1, size: tileSize)
-        let red = SKTileDefinition(texture: tile2, size: tileSize)
-        
-        let tileGroup1 = SKTileGroup(tileDefinition: black)
-        let tileGroup2 = SKTileGroup(tileDefinition: red)
-        
-        let tileSet = SKTileSet(tileGroups: [tileGroup1,tileGroup2])
-        
-        let tileMap = SKTileMapNode(tileSet: tileSet, columns: columns, rows: rows, tileSize: tileSize)
-        
-        for c in 0..<tileMap.numberOfColumns {
-            for r in 0..<tileMap.numberOfRows {
-                for i in rooms {
-                    // iterate through each room and carve it out
-                    if i.x1 <= c && i.x2 >= c && i.y1 <= r && i.y2 >= r {
-                        tileMap.setTileGroup(tileGroup2, forColumn: c, row: r)
-                    } else if tileMap.tileGroup(atColumn: c, row: r) != tileGroup2 && tileMap.tileGroup(atColumn: c, row: r) != tileGroup3 {
-                        tileMap.setTileGroup(tileGroup1, forColumn: c, row: r)
-                    }
-                }
-                for h in hallways {
-                    // iterate through each hallway and carve it out
-                    if h.x1 <= c && h.x2 >= c && h.y1 <= r && h.y2 >= r {
-                        tileMap.setTileGroup(tileGroup2, forColumn: c, row: r)
-                    } else if tileMap.tileGroup(atColumn: c, row: r) != tileGroup2 && tileMap.tileGroup(atColumn: c, row: r) != tileGroup3 {
-                        tileMap.setTileGroup(tileGroup1, forColumn: c, row: r)
-                    }
-                }
-            }
-        }
-        
-        self.addChild(tileMap)
-        tileMap.setScale(0.2)
-    }
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func drawRoom(roomRect: Room) {
-        rooms.append(roomRect)
-    }
-    func drawHall(hallRect: [Room]) {
-        for rect in hallRect {
-            hallways.append(rect)
-        }
-    }
 }
 
 class Leaf {
@@ -228,11 +119,11 @@ class Leaf {
         // Connects 2 rooms together with hallways
         hallways = [];
     
-        // get width and height of first room
+        // Get width and height of first room
         let point1 = point(x: Int.random(in: (left.x1 + 1)..<(left.x2 - 1)),
                            y: Int.random(in: (left.y1 + 1)..<(left.y2 - 1)))
         
-        // get width and height of second room
+        // Get width and height of second room
         let point2 = point(x: Int.random(in: (right.x1 + 1)..<(right.x2 - 1)),
                            y: Int.random(in: (right.y1 + 1)..<(right.y2 - 1)))
         
@@ -240,18 +131,18 @@ class Leaf {
             // Horizontally first, then vertically:
             // From point1 to min(point2.x, point1.y):
             hallways.append(Room(X: min(point1.x, point2.x), Y: point1.y,
-                                 W: Int(abs(point1.x - point2.x)), H: 1))
+                                 W: Int(abs(point1.x - point2.x)), H: 0))
             // From (point2.x, point1.y) to point2:
             hallways.append(Room(X: point2.x, Y: min(point1.y, point2.y),
-                                 W: 1, H: Int(abs(point1.y - point2.y))))
+                                 W: 0, H: Int(abs(point1.y - point2.y))))
         } else {
             // Vertically first, then Horizontally:
             // From point1 to min(point1.x, point2.y):
             hallways.append(Room(X: point1.x, Y: min(point1.y, point2.y),
-                                 W: 1, H: Int(abs(point1.y - point2.y))))
+                                 W: 0, H: Int(abs(point1.y - point2.y))))
             // From (point1.x, point2.y) to point2:
             hallways.append(Room(X: min(point1.x, point2.x), Y: point2.y,
-                                 W: Int(abs(point1.x - point2.x)), H: 1))
+                                 W: Int(abs(point1.x - point2.x)), H: 0))
         }
     }
 }
@@ -271,6 +162,5 @@ class Room {
         center = CGPoint(x: (x1 + x2) / 2, y: (y1 + y2) / 2)
     }
 }
-
 
 
